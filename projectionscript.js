9263217f6b6159150f0d294aa7b0b497ef2987af
Luca Kler Lago <66525361+lucalago@users.noreply.github.com>
2020-12-07 18:10:00 +0100

    Update

---
+++ projectionscript.js
@@ -0,0 +1,198 @@
+var currentProjection;
+
+// EXECUTE 0 - CHOOSE PATH FREE EXPLORE / GUIDED
+function loadWelcomeModule (){
+    document.getElementById('proj-welcome').style.display = 'block';
+    currentProjection = 'proj-welcome';
+}
+
+function killWelcomeModule (){
+    document.getElementById('proj-welcome').style.display = 'none';
+}
+
+// EXECUTE 1 - INTRODUCTION - CIRCULARITY
+function loadIntroductionModule (){
+    document.getElementById('proj-intro-concept').style.display = 'block';
+    currentProjection = 'proj-intro-concept';
+}
+
+function killIntroductionModule (){
+    document.getElementById('proj-intro-concept').style.display = 'none';
+}
+
+// EXECUTE 2 - CITIZEN QUOTES - WOULD YOU DO THIS?
+function loadCitizenModule (){
+    document.getElementById('proj-citizen-quotes').style.display = 'block';
+    currentProjection = 'proj-citizen-quotes';
+}
+
+function killCitizenModule (){
+    document.getElementById('proj-citizen-quotes').style.display = 'none';
+}
+
+// EXECUTE 3 - INTRODUCING 3 VALUE CHAINS
+function loadIntroValueChainsModule (){
+    document.getElementById('proj-value-chains').style.display = 'block';
+    currentProjection = 'proj-value-chains';
+}
+
+function killIntroValueChainsModule (){
+    document.getElementById('proj-value-chains').style.display = 'none';
+}
+
+// EXECUTE 4 - BROAD DATA OF 3 VALUE CHAINS
+function loadBroadDataVcsModule (){
+    document.getElementById('proj-broad-data-vcs').style.display = 'block';
+    currentProjection = 'proj-broad-data-vcs';
+}
+
+function killBroadDataVcsModule (){
+    document.getElementById('proj-broad-data-vcs').style.display = 'none';
+}
+
+// EXECUTE 5 - CONSTRUCTION SECTOR
+function loadConstructionSectorModule (){
+    document.getElementById('proj-construction-sector').style.display = 'block';
+    currentProjection = 'proj-construction-sector';
+}
+
+function killConstructionSectorModule (){
+    document.getElementById('proj-construction-sector').style.display = 'none';
+}
+
+// EXECUTE 6 - FOOD SECTOR
+function loadFoodSectorModule (){
+    document.getElementById('proj-food-sector').style.display = 'block';
+    currentProjection = 'proj-food-sector';
+}
+
+function killFoodSectorModule (){
+    document.getElementById('proj-food-sector').style.display = 'none';
+}
+
+// EXECUTE 7 - CONSUMER GOODS SECTOR
+function loadConsumerGoodsSectorModule (){
+    document.getElementById('proj-consumer-goods-sector').style.display = 'block';
+    currentProjection = 'proj-consumer-goods-sector';
+}
+
+function killConsumerGoodsSectorModule (){
+    document.getElementById('proj-consumer-goods-sector').style.display = 'none';
+}
+
+// EXECUTE 8 - SPECULATIVE FUTURE
+function loadSpectulativeFutureModule (){
+    document.getElementById('proj-speculative-future').style.display = 'block';
+    currentProjection = 'proj-speculative-future';
+}
+
+function killSpectulativeFutureModule (){
+    document.getElementById('proj-speculative-future').style.display = 'none';
+}
+
+// EXECUTE 9 - COMMITMENT TO FUTURE
+function loadCommitFutureModule (){
+    document.getElementById('proj-commit-future').style.display = 'block';
+    currentProjection = 'proj-commit-future';
+}
+
+function killCommitFutureModule (){
+    document.getElementById('proj-commit-future').style.display = 'none';
+}
+
+// EXECUTE 10 - THANK YOU / HOW TO GET INVOLVED / BACK TO START
+function loadGetInvolvedModule (){
+    document.getElementById('proj-get-involved').style.display = 'block';
+    currentProjection = 'proj-get-involved';
+}
+
+function killGetInvolvedModule (){
+    document.getElementById('proj-get-involved').style.display = 'none';
+}
+
+// EXECUTE 11 - MAIN PAGE DATA DISCOVERY
+function loadDataDashboardModule (){
+    document.getElementById('proj-data-dashboard').style.display = 'block';
+    currentProjection = 'proj-data-dashboard';
+}
+
+function killDataDashboardModule (){
+    document.getElementById('proj-data-dashboard').style.display = 'none';
+}
+
+// function to load welcome screen
+
+function startWelcomeModule(){
+    loadWelcomeModule();
+}
+
+// functions for Guided Experience
+
+function startIntroductionModule(){
+    killWelcomeModule();
+    loadIntroductionModule();
+}
+
+function startCitizenQuotesModule(){
+    killIntroductionModule();
+    loadCitizenModule();
+}
+
+function startIntroValueChainsModule(){
+    killCitizenModule();
+    loadIntroValueChainsModule();
+}
+
+function startBroadDataVcsModule(){
+    killIntroValueChainsModule();
+    loadBroadDataVcsModule();
+}
+
+function startConstructionSectorModule(){
+    killBroadDataVcsModule();
+    loadConstructionSectorModule();
+}
+
+function startFoodSectorModule(){
+    killConstructionSectorModule();
+    loadFoodSectorModule();
+}
+
+function startConsumerGoodsSectorModule(){
+    killFoodSectorModule();
+    loadConsumerGoodsSectorModule();
+}
+
+function startSpeculativeFutureModule(){
+    killConsumerGoodsSectorModule();
+    loadSpeculativeFutureModule();
+}
+
+function startCommitFutureModule(){
+    killSpeculativeFutureModule();
+    loadCommitFutureModule();
+}
+
+function startGetInvolvedModule(){
+    killCommitFutureModule();
+    loadGetInvolvedModule();
+}
+
+function restartWelcomeModule(){
+    killMConsumerGoodsSectorodule();
+    loadWelcomeModule();
+}
+
+// functions of discovery data section
+
+function startDataDashboardModule(){
+    killWelcomeModule();
+    loadDataDashboardModule();
+}
+
+// reset projection
+
+function resetProjection(){
+    document.getElementById(currentProjection).style.display = "none";
+    loadWelcomeModule()
+}
---
+++ touchscreenscript.js
@@ -0,0 +1,143 @@
+var currentSectionId = 'init';
+var homeSection = document.getElementById("welcome");
+
+let sections = document.getElementsByTagName("section");
+for (let section of sections) {
+  section.style.display = 'none';
+}
+document.getElementById(currentSectionId).style.display = "block";
+
+let links = document.getElementsByClassName("nav-button");
+
+for (let link of links){
+    link.addEventListener('click', function(e) {
+      var newSectionId = link.getAttribute("href").slice(1, link.getAttribute("href").length);
+
+      var newSection = document.getElementById(newSectionId);
+      var currentSection = document.getElementById(currentSectionId);
+
+      currentSection.style.display = 'none';
+      newSection.style.display = 'block';
+      currentSectionId = newSectionId;
+    })
+};
+
+// HELP SECTION FUNCTIONALITY
+
+let helpSection = document.getElementById('help-dialogue');
+let helpButton = document.getElementById('help-btn');
+let closeHelpButton = document.getElementById('close-help-btn');
+
+function showHelpDialogue() {
+    helpSection.style.width = "50%";
+}
+
+function hideHelpDialogue() {
+    helpSection.style.width = "0";
+}
+
+helpButton.addEventListener('click', showHelpDialogue);
+closeHelpButton.addEventListener('click', hideHelpDialogue);
+
+// HOME BUTTON FUNCTIONALITY
+
+let homeButton = document.getElementById('home-btn');
+let goHomeButton = document.getElementById('confirm-home-btn');
+let closeHomeButton = document.getElementById('close-home-btn');
+let homeDialogue = document.getElementById('home-dialogue');
+
+homeButton.addEventListener('click', showHomeDialogue);
+closeHomeButton.addEventListener('click', hideHomeDialogue);
+goHomeButton.addEventListener('click', goHome);
+
+function showHomeDialogue() {
+    homeDialogue.style.display = "block";
+}
+
+function hideHomeDialogue() {
+    homeDialogue.style.display ="none";
+}
+
+function goHome() {
+    homeDialogue.style.display ="none";
+    document.getElementById(currentSectionId).style.display = "none";
+    document.getElementById('welcome').style.display = "block";
+    currentSectionId = 'welcome';
+    win2.resetProjection();
+}
+
+
+//
+// START OF SECONDARY WINDOW INTERACTION CODE
+//
+// Sets all the settings and the functions that must be run by the projectionscript.js
+
+var win2;
+var windowFeatures = 'scrollbars=no, toolbar=no, menubar=no,width=1280,height=800,left=0,top=0';
+
+function openProjectionWindow() {
+    if(win2 == null || win2.closed){
+        win2 = window.open('index2.html', 'projectionWindow', windowFeatures);
+    } else {
+        win2.focus();
+    }
+}
+
+function initFunction(){
+    openProjectionWindow();
+    startWelcomeModule();
+}
+
+// INITIALIZE ENTIRE SETUP
+// EXECUTE 0 - CHOOSE PATH FREE EXPLORE / GUIDED
+let initButton = document.getElementById('init-btn');
+initButton.addEventListener('click', initFunction);
+
+// EXECUTE 1 - INTRODUCTION - CIRCULARITY
+let introButton = document.getElementById('intro-btn');
+introButton.addEventListener('click', function(){win2.startIntroductionModule()});
+
+// EXECUTE 2 - CITIZEN QUOTES - WOULD YOU DO THIS?
+let citizenQuotesButton = document.getElementById('citizen-quotes-btn');
+citizenQuotesButton.addEventListener('click', function(){win2.startCitizenQuotesModule()});
+
+// EXECUTE 3 - INTRODUCING 3 VALUE CHAINS
+let introduceValueChainsButton = document.getElementById('introduce-value-chains-btn');
+introduceValueChainsButton.addEventListener('click', function(){win2.startIntroValueChainsModule()});
+
+// EXECUTE 4 - BROAD DATA OF 3 VALUE CHAINS
+let broadDataVcsButton = document.getElementById('broad-data-vcs-btn');
+broadDataVcsButton.addEventListener('click', function(){win2.startBroadDataVcsModule()});
+
+// EXECUTE 5 - CONSTRUCTION SECTOR
+let constructionSectorButton = document.getElementById('construc-sector-btn');
+constructionSectorButton.addEventListener('click', function(){win2.startConstructionSectorModule()});
+
+// EXECUTE 6 - FOOD SECTOR
+let foodSectorButton = document.getElementById('food-sector-btn');
+foodSectorButton.addEventListener('click', function(){win2.startFoodSectorModule()});
+
+// EXECUTE 7 - CONSUMER GOODS SECTOR
+let consumerGoodsButton = document.getElementById('consumer-goods-btn');
+consumerGoodsButton.addEventListener('click', function(){win2.startConsumerGoodsSectorModule()});
+
+// EXECUTE 8 - SPECULATIVE FUTURE
+let speculativeFutureButton = document.getElementById('speculative-future-btn');
+speculativeFutureButton.addEventListener('click', function(){win2.startSpeculativeFutureModule()});
+
+// EXECUTE 9 - COMMITMENT TO FUTURE
+let commitFutureButton = document.getElementById('commit-future-btn');
+commitFutureButton.addEventListener('click', function(){win2.startCommitFutureModule()});
+
+// EXECUTE 10 - THANK YOU / HOW TO GET INVOLVED / BACK TO START
+let getInvolvedButton = document.getElementById('get-involved-btn');
+getInvolvedButton.addEventListener('click', function(){win2.startGetInvolvedModule()});
+
+// EXECUTE 11 - MAIN PAGE DATA DISCOVERY
+let dataDiscoveryButton = document.getElementById('data-discovery-btn');
+dataDiscoveryButton.addEventListener('click', function(){win2.startDataDashboardModule()});
+
+
+// remember to add number to the end of the BTN so you can extract that
+// from an array of items, to facilitate the code
+// loop for the button pressing
